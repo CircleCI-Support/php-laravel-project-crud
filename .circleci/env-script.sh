9af7177913939b59e26d5ef449fc222404beaf9e
johanna <11861843+jkzilla@users.noreply.github.com>
2023-11-27 20:49:53 -0800

    the string '{9be0f072-80cd-4c6e-8f3d-0a4b2a42eac6}' is not a valid UUID

--- .circleci/env-script.sh
+++ .circleci/env-script.sh
@@ -9,24 +9,23 @@ set +a
 
 printenv
 
-response=$(curl -X POST --url 'https://circleci.com/api/v2/context' \
-    -H 'Circle-Token: f7aa3b819623c1cc786dd39bb9e09742c47fea3c' \ 
-    -H 'content-type: application/json' \ 
-    -d '{"name":"php-context2","owner":{"id":"5d18d3c7-f8c4-4c5f-a691-b730e67047d3","type":"organization"}}' --write-out '%{http_code}' --silent --output /dev/null servername)
-context_id=${ response | jq -r '.id' }
-echo "Context ID: $context_id"
-# env | while IFS= read -r line; do
-#   value=${line#*=}
-#   name=${line%%=*}
-#   echo "V: $value"
-#   echo "N: $name"
-# # "message" : "Invalid JSON body." nov 27
-#     curl --request PUT \
-#     --url https://circleci.com/api/v2/context/%7B$context_id%7D/environment-variable/$name \
-#     -H 'Circle-Token: f7aa3b819623c1cc786dd39bb9e09742c47fea3c'  \
-#     -H 'content-type: application/json' \
-#     --data '{"value":$value}'
-# done
+response=$(curl -X POST --url 'https://circleci.com/api/v2/context' -H 'Circle-Token: f7aa3b819623c1cc786dd39bb9e09742c47fea3c' -H 'Content-Type: application/json' -d '{"name":"php-context4","owner":{"id":"5d18d3c7-f8c4-4c5f-a691-b730e67047d3","type":"organization"}}')
+echo $response
+context_id=$(echo $response | jq -r '.id')
+env | while IFS= read -r line; do
+  value=${line#*=}
+  name=${line%%=*}
+  echo "V: $value"
+  echo "N: $name"
+
+#   "message" : "Invalid parameter: the string '{9be0f072-80cd-4c6e-8f3d-0a4b2a42eac6}' is not a valid UUID"
+
+    curl --request PUT \
+    --url https://circleci.com/api/v2/context/%7B$context_id%7D/environment-variable/$name \
+    -H 'Circle-Token: f7aa3b819623c1cc786dd39bb9e09742c47fea3c'  \
+    -H 'content-type: application/json' \
+    --data '{"value":"$value"}'
+done
 
 # Create new context for PHP application environment variables
 
